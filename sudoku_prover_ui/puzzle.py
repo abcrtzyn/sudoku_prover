da03d4ef7bdce62bbd814b7642f8756405a78764
Aidan Christensen <aidan_christensen@icloud.com>
2026-03-18 13:08:35 -0500

    WIP can get it to parse something

--- sudoku_prover_ui/puzzle.py
+++ sudoku_prover_ui/puzzle.py
@@ -1,87 +1,52 @@
 
 
-import ast
 from dataclasses import dataclass
 from io import TextIOWrapper
+import sys
 from typing import Dict, List, Tuple
 
-def expect(s: str,line: str):
-    """expects a string s, if it doesn't get it, error out"""
-    if s != line:
-        raise ValueError(f"expected {s} but got {line}")
+from lark import Lark
+from lark.visitors import Interpreter
 
-def parse_cell_layout(line: str) -> List[Tuple[int,int]]:
-    # if we switch to a multiline format, we need to inculed the file pointer
-    raise NotImplementedError('parse_cell_layout is not implemented yet.')
+class SukoInterpreter(Interpreter):
+    _cell_count: int | None
+    _cell_layout: List[Tuple[int,int]] | None
+    _symbols: str | None
+    _constraints: Dict[str,str]
+    
+    def __init__(self,is_puzzle:bool=True):
+        self.is_puzzle = is_puzzle
+        self._cell_count = None
+        self._cell_layout = None
+        self._symbols = None
+        self._constraints = {}
 
-def parse_imported_constraints(fp) -> Dict[str,str]:
-    # basically this is reading another template file, but the file should not have any other field than imported constraints or constraints
-    raise NotImplementedError('parse_imported_constraints is not implemented yet.')
+    def suko(self, tree): # type: ignore
+        # go parse the tree
+        self.visit_children(tree) # type: ignore
 
+        if self._cell_count is None:
+            raise ValueError('No cell_count was given')
+        if self._cell_layout is None:
+            raise ValueError('No cell_layout was given')
+        if len(self._cell_layout) != self._cell_count:
+            raise ValueError('Number of cells in cell_layout did not match cell_count')
+        if self._symbols is None:
+            raise ValueError('No symbols object was provided')
+        # TODO check that symbols can be found in lean
+        # any other data validation we need
 
+        return Puzzle(self._cell_count,self._cell_layout,self._symbols,self._constraints)
 
-
-@dataclass
-class Template:
-    """Template represents a potential puzzle, one that is merely unfinished
-    It may even include it's own templates..."""
-    cell_count: int | None
-    cell_layout: List[Tuple[int,int]] | None
-    symbols: str | None
-    constraints: Dict[str,str]
-
-    @staticmethod
-    def import_from_file(fp: TextIOWrapper) -> Template:
-        """Reads everything from DEFINITION to an unknown thing or EOF"""
-        expect("DEFINITION",fp.readline().strip().upper())
-        template: Template | None = None
-        cell_count: int | None= None
-        cell_layout: List[Tuple[int,int]] | None = None
-        symbols: str | None = None
-        constraints: Dict[str,str] = {}
-
-        while True:
-            line = fp.readline().rstrip()
-            # should be looking at key value pairs now (mostly)
-            line_items = line.split()
-            key = line_items[0]
-            match key:
-                case "template":
-                    file_path = line.removeprefix('template').strip()
-                    template = Template.import_from_file(file_path)
-                case "cell_count":
-                    cell_count = int(line.removeprefix('cell_count').strip())
-                    if cell_count < 0:
-                        raise ValueError(f"cell_count must be positive, I think organisms need at least one cell to do anything? line is {line}")
-                case "cell_layout":
-                    cell_layout = parse_cell_layout(line.removeprefix('cell_layout').strip())
-                case "symbols":
-                    symbols = ast.literal_eval(line.removeprefix('symbols').strip())
-                case "imported_constraints":
-                    imp_cons = parse_imported_constraints(fp)
-                    for c in imp_cons:
-                        if c in constraints:
-                            ValueError(f'duplicate constraint name {c} imported as {imp_cons[c]} and existing as {constraints[c]}')
-                        constraints[c] = imp_cons[c]
-                case ""
+    def template_section(self, tree): #type: ignore
 
 
 
 
-                case _:
-                    raise ValueError(f"don't know what to do with a the key {key} in line {line}")
+
+    # def cell_count(self,tree):
 
 
-
-    @staticmethod
-    def import_from_file_path(file_path: str) -> Template:
-        """This reads an entire template file"""
-        with open(file_path) as fp:
-            expect("TEMPLATE",fp.readline().strip().upper())
-            temp = Template.import_from_file(fp)
-            # TODO check that proof does not appear
-        return temp
-        
 
 
 @dataclass
@@ -91,16 +56,45 @@ class Puzzle:
     symbols: str
     constraints: Dict[str,str]
 
-    
     @staticmethod
-    def import_from_file(fp: TextIOWrapper) -> Puzzle:
-        """This reads everything starting from DEFINITION to PROOF"""
-        puzzle_temp = Template.import_from_file(fp)
-        # check that puzzle temp has all the required fields.
-        # then return
-        this = Puzzle()
+    def import_puzzle(text: str):
+        """text is from beginning of file to proof"""
+        parser = Lark.open('suko.lark', parser='lalr',start='suko') #type:ignore
+        tree = parser.parse(text) #type:ignore
+
+        interpreter = SukoInterpreter()
+        puzzle = interpreter.visit(tree) #type:ignore
+        return puzzle
+
+        
+        
 
     def generate_lean_structure(self) -> str:
         raise NotImplementedError()
 
 
+def main(argv: List[str]):
+    """attempts to import the puzzle file provided by command line args
+    used for testing purposes"""
+    if len(argv) > 1:
+        file = argv[1]
+    else:
+        print('provide a file to parse as an argument')
+        exit()
+    
+    with open(file,'r') as f:
+        text = f.read()
+
+    parts = text.split("PROOF", 1)
+    definition_text = parts[0]
+    proof_text = parts[1] if len(parts) > 1 else ""
+
+    puzzle = Puzzle.import_puzzle(definition_text)
+    print(puzzle)
+
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
