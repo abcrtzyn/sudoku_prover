6d14356122eb784a1b1b9f19bfb565e2a8d41730
Aidan Christensen <aidan_christensen@icloud.com>
2026-03-29 21:25:19 -0500

    file parser works with new variables

--- sudoku_prover_ui/puzzle.py
+++ sudoku_prover_ui/puzzle.py
@@ -35,31 +35,33 @@ class Puzzle:
             raise ValueError(f'No python implementation of {self.symbols}')
         self.symbols_python = SYMBOLS_DICT[self.symbols]
 
-        self.qualified_constraints_python = {}
+        self.pythonized_constraints = {}
         # find python definitions for the constraints
-        for c,code in self.qualified_constraints.items():
-            if mat := re.match(r'f\s+(\d+)\s*=\s*(\w+)',code):
-                # given digit constraint
-                symbol = mat.group(2)
-                if symbol.isnumeric():
-                    symbol = int(symbol)
-                if symbol not in self.symbols_python:
-                    raise ValueError(f'Could not unify {symbol} with anything in {self.symbols_python}')
-                self.qualified_constraints_python[c] = ('Given',(int(mat.group(1)),symbol))
-            elif mat := re.match(r'UniqueSet\s+f\s+({\s*\d+\s*(,\s*\d+\s*)*})',code):
-                # UniqueSet
-                cells = list(map(int, [item.strip() for item in mat.group(1).strip('{ }').split(',')]))
-                self.qualified_constraints_python[c] = ('UniqueSet',cells)
-            else:
-                # unknown constraint type
-                raise ValueError(f'No python implementation of the lean code "{code}", it could also not have parsed correctly')
+        for constraints_dict in [self.puzzle_level_constraints, self.imported_constraints]:
+            for c,code in constraints_dict.items():
+                if mat := re.match(r'f\s+(\d+)\s*=\s*(\w+)',code):
+                    # given digit constraint
+                    symbol = mat.group(2)
+                    if symbol.isnumeric():
+                        symbol = int(symbol)
+                    if symbol not in self.symbols_python:
+                        raise ValueError(f'Could not unify {symbol} with anything in {self.symbols_python}')
+                    self.pythonized_constraints[c] = ('Given',(int(mat.group(1)),symbol))
+                elif mat := re.match(r'UniqueSet\s+f\s+({\s*\d+\s*(,\s*\d+\s*)*})',code):
+                    # UniqueSet
+                    cells = list(map(int, [item.strip() for item in mat.group(1).strip('{ }').split(',')]))
+                    self.pythonized_constraints[c] = ('UniqueSet',cells)
+                else:
+                    # unknown constraint type
+                    raise ValueError(f'No python implementation of the lean code "{code}", it could also not have parsed correctly')
 
 
     def generate_lean_structure(self) -> str:
         definition = f"structure Puzzle (f: Nat -> {self.symbols}) where\n"
         definition += f"  outside_grid: ∀ x, x ≥ {self.cell_count} -> f x = {self.symbols_python[0]}\n"
-        for name, constraint in self.constraints.items():
-            definition += f"  {name}: {constraint}\n"
+        for constraint_dict in [self.import_constraints,self.imported_constraints]:
+            for name, constraint in constraint_dict.items():
+                definition += f"  {name}: {constraint}\n"
             
 
         return definition
